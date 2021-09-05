4b89bde123c451f95f0ae6780c77e63175f36f00
dandeduck <danielmkhlv@gmail.com>
2021-09-05 21:09:19 +0300

    Clean Obstructing

--- Assets/Scripts/Lib/Obstruction/Obstructing.cs
+++ Assets/Scripts/Lib/Obstruction/Obstructing.cs
@@ -63,6 +63,12 @@ public class Obstructing : MonoBehaviour
 
     private void SetupMaterialBlendMode(Material material)
     {
+        SetAlphaClip(material);
+        SetMaterialProperties(material);
+    }
+
+    private void SetAlphaClip(Material material)
+    {
         bool alphaClip = material.GetFloat("_AlphaClip") == 1;
 
         if (alphaClip)
@@ -70,28 +76,37 @@ public class Obstructing : MonoBehaviour
 
         else
             material.DisableKeyword("_ALPHATEST_ON");
+    }
 
+    private void SetMaterialProperties(Material material)
+    {
         SurfaceType surfaceType = (SurfaceType) material.GetFloat("_Surface");
 
         if (surfaceType == SurfaceType.Opaque)
-        {
-            material.SetOverrideTag("RenderType", "");
-            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-            material.SetInt("_ZWrite", 1);
-            material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-            material.renderQueue = -1;
-        }
+            SetupOpaqueMaterial(material);
 
-        else
-        {//only setup for alpha blend mode -> https://answers.unity.com/questions/1608815/change-surface-type-with-lwrp.html
-            material.SetOverrideTag("RenderType", "Transparent");
-            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            material.SetInt("_ZWrite", 0);
-            material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-            material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-            material.SetShaderPassEnabled("ShadowCaster", false);
-        }
+        else // -> https://answers.unity.com/questions/1608815/change-surface-type-with-lwrp.html
+            SetupAlphaTransparentMaterial(material);
+    }
+
+    private void SetupOpaqueMaterial(Material material)
+    {
+        material.SetOverrideTag("RenderType", "");
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        material.SetInt("_ZWrite", 1);
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = -1;
+    }
+
+    private void SetupAlphaTransparentMaterial(Material material)
+    {
+        material.SetOverrideTag("RenderType", "Transparent");
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_ZWrite", 0);
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        material.SetShaderPassEnabled("ShadowCaster", false);
     }
 }
