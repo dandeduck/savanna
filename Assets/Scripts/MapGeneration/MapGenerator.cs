ff25ba157acbaf355fd574a88e995ec19ea1f858
dandeduck <danielmkhlv@gmail.com>
2021-09-10 17:43:48 +0300

    Add Color Rendering to Terrain

--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColorMap};
+    public DrawMode drawMode;
+
     public bool autoUpdate;
 
     [SerializeField] private int mapWidth;
@@ -17,12 +20,27 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private Vector2 offset;
 
     [SerializeField] private MapDisplay display;
+    [SerializeField] private Gradient terrains;
 
     public void GenerateMap()
     {
         float [,] noiseMap = ComputeUtil.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[mapWidth * mapHeight];
 
-        display.DrawnNoiseMap(noiseMap);
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float height = noiseMap[x, y];
+
+                colorMap[y * mapWidth + x] = terrains.Evaluate(height);
+            }
+        }
+
+        if (drawMode == DrawMode.NoiseMap)
+            display.DrawTexture(GraphicsUtil.TextureFromHeightMap(noiseMap));
+        else if (drawMode == DrawMode.ColorMap)
+            display.DrawTexture(GraphicsUtil.TextureFromColorMap(colorMap, mapWidth, mapHeight));
     }
 
     private void OnValidate()
